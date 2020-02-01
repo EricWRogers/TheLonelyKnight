714a54394384a1a3a14a06ee373f58810be80b6a
Caleb Dan Gammons <alldayhat@hotmail.com>
2020-02-01 09:09:57 -0600

    Created Wave function

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -5,6 +5,21 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+    //Create gameobjects to hold the three types of enemies we will be instantiating.
+    public GameObject enemy;
+
+    //Wave Timer for counting down the 2 minutes inbetween waves.
+    private float WTimer = 7200;
+
+    //Set up WaveNumber we are on.
+    //Initially Wave Number is set to zero at start because we wait two minutes before the first wave.
+    private int WaveNumber = 0;
+
+    //Set up int for the number of enemies we are spawning.
+    private int NumberEnemiesToSpawn = 0;
+
+    //Set up private int value for the current number of enemies we have spawned.
+    private int NumberEnemiesCurrentlySpawned = 1;
 
     //Creates a new event.
     public UnityEvent m_Death = new UnityEvent();
@@ -12,9 +27,16 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+
+        //Invoke the Spawner
+        InvokeRepeating ("Wave", WTimer, WTimer);
+
+        //Listeners for generic functions
         m_Death.AddListener(MyAction);
         m_Messages.AddListener(MyMessages);
+
     }
+
 
     //The private float value of scrap parts the player collects to fix things.
     private float ScrapCount;
@@ -70,6 +92,28 @@ public class GameManager : MonoBehaviour
     {
         ScrapCount -= Num;
     }
+
+    public void Wave()
+    {
+            //work on all spawning enemies and waves here.
+
+            //Increase the number of the wave we are currently on.
+            WaveNumber+=1;
+
+            //calculate the new number of Enemies to spawn based on the algorthym provided by Brandon, Eric, and Justin.
+            NumberEnemiesToSpawn = WaveNumber * 4 + 2;
+
+            //Spawn the enemy using the calculations we have just done.
+            while(NumberEnemiesCurrentlySpawned <= NumberEnemiesToSpawn){
+                Instantiate (enemy, Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+                NumberEnemiesCurrentlySpawned += 1;
+            }
+            
+            NumberEnemiesCurrentlySpawned = 1;
+
+    }
+
+
 
 
 
