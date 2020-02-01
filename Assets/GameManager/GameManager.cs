34400cbedf87ebcec659ae57a4289e11d798afe6
Caleb Dan Gammons <alldayhat@hotmail.com>
2020-02-01 05:15:29 -0600

    Updated GameManager again

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -1,5 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+
+    //Creates a new event.
+    public UnityEvent m_Death = new UnityEvent();
+
+    void Start()
+    {
+        m_Death.AddListener(MyAction);
+    }
+
+    //The private float value of scrap parts the player collects to fix things.
+    private float ScrapCount;
+
+    //The four private floats for Turrets.
+    private float Turret1;
+    private float Turret2;
+    private float Turret3;
+    private float Turret4;
+
+    //The private float value of player's health.
+    private float PlayerHealth;
+
+    //The private float value of the castle health.
+    private float CastleHealth;
+
+    //The public float value which gets the private float value of ScrapCount.
+    public float scrpcont { get { return ScrapCount; } }
+
+    //The public float value which gets the private float value of Turrets 1-4.
+    public float turr1 { get { return Turret1; } }
+    public float turr2 { get { return Turret2; } }
+    public float turr3 { get { return Turret3; } }
+    public float turr4 { get { return Turret4; } }
+
     //The public float value which gets the private float value of PlayerHealth.
     public float PlyrHealth { get { return PlayerHealth; if(PlayerHealth == 0){m_Death.Invoke();}} }
 
-    public float CstlHealth { get { return CastleHealth; if(CastleHealth == 0){m_Death.Invoke();} } }
     //The public float value which gets the private float value of PlayerHealth.
+    public float CstlHealth { get { return CastleHealth; if(CastleHealth == 0){m_Death.Invoke();} } }
+
+//-------------------------------------------------------------------------------------
+
+    void MyAction()
+    {
+        //Handle the death screen popup here.
+
+    }
+
+
+
+
+}
