9f5ff7d6c96ae750370ae1f05d1e5f21509e5f07
Justin Liles <justin.liles@live.com>
2020-02-01 00:42:29 -0600

    Updated Player Movement and added Camera Controller Script

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Range(0.5f, 3f)] public float mouseSensitivivty = 1.5f;
+    public float Xmax = 30.0f;
+    public float Xmin = -30.0f;
+    public float speed = 100f;
+    public float maxAngle = 20f;
+
+    float xAxisClamp = 0.0f;
+
+    Vector3 camCenter;
+
+    GameObject Player;
+
+    void Awake()
+    {
+        camCenter = transform.localPosition;
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 left = -transform.right;
+        Vector3 right = transform.right;
+
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");;
+
+        float rotAmountX = MouseX * mouseSensitivivty;
+        float rotAmountY = MouseY * mouseSensitivivty;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = Player.transform.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if(xAxisClamp > Xmax)
+        {
+            xAxisClamp = Xmax;
+            targetRotCam.x = Xmax;
+        }
+        else if (xAxisClamp < Xmin)
+        {
+            xAxisClamp = Xmin;
+            targetRotCam.x = Xmin;
+        }
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        Player.transform.rotation = Quaternion.Euler(targetRotBody);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    float originalSpeed;
+    float gravityHolder;
+
+    Vector3 moveDirection = Vector3.zero;
+    Vector3 playerSize;
+
+    CharacterController characterController;
+
+    void Start()
+    {
+        playerSize = transform.localScale;
+        gravityHolder = gravity;
+        originalSpeed = speed;
+
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            Crouching();
+            Movement();
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void Crouching()
+    {
+        if(Input.GetButton("Crouch"))
+        {
+            gravity = gravityHolder * 10f; 
+            Vector3 sizeHolder = playerSize;
+            sizeHolder.y /= 3f;
+            transform.localScale = sizeHolder;
+        }
+        else
+        {
+            gravity = gravityHolder;
+            Vector3 sizeHolder = playerSize;
+            if(transform.localScale != playerSize)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, sizeHolder, 0.3f);
+            }
+        }
+    }
+
+    void Movement()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection = moveDirection * speed;
+    }
+}
