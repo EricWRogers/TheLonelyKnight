a9334ead212d9c40f7b68635745f04bd3dfece51
Caleb Dan Gammons <alldayhat@hotmail.com>
2020-02-02 05:45:34 -0600

    Removed Debug logs from scripts for optimization.

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -143,7 +143,6 @@ public class GameManager : MonoBehaviour
     {
         StateChanger();
         PlayerWaitedRestore();
-        Debug.Log(scrapCount);
     }
 
     //-------------------------------------------------------------------------------------
