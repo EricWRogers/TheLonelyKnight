7c0acf41ba50effe26023bb1191136e921a3cedf
Caleb Dan Gammons <alldayhat@hotmail.com>
2020-02-02 05:53:44 -0600

    Took out unnessessary turret code from Game Manager.

--- Assets/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager.cs
@@ -63,12 +63,6 @@ public class GameManager : MonoBehaviour
 
     private float ScrapCount;
 
-    //The four private floats for Turrets.
-    private float Turret1;
-    private float Turret2;
-    private float Turret3;
-    private float Turret4;
-
     //A Timer value for the Max number of time before the player starts to heal.
     private float OrigWaitedTime = 10;
 
@@ -83,12 +77,6 @@ public class GameManager : MonoBehaviour
 
     //The public float value which gets the private float value of ScrapCount.
     public float scrapCount { get { return ScrapCount; } }
-
-    //The public float value which gets the private float value of Turrets 1-4.
-    public float turr1 { get { return Turret1; } }
-    public float turr2 { get { return Turret2; } }
-    public float turr3 { get { return Turret3; } }
-    public float turr4 { get { return Turret4; } }
 
     //The public float value which gets the private float value of PlayerHealth.
     public float playrHealth { get { return PlayerHealth; } }
