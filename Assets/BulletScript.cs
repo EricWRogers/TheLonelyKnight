20c2efba9893ee35c0b0edf4b51152e9ff6f65a7
Justin Liles <justin.liles@live.com>
2020-02-02 12:30:54 -0600

    Updated Gun

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public int BulletSpeed = 50;
+
+    public Rigidbody rb;
+
+	void Update () 
+    {
+        rb.position = (Vector3.right * Time.deltaTime * BulletSpeed);
+	}
+
     void OnTriggerEnter(Collider other) 
     {
         if(other.transform.tag == "Enemy")
         {
+
             other.transform.GetComponent<EnemyHealth>().TakeDamage(15);
         }
 
-        Destroy(gameObject);
+        //Destroy(gameObject, 1f);
     }
 }
